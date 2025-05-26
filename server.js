30bfd98e0081f1ff349fbfac167b7d95e0d9ef80
Ricardo <rick0709xe@gmail.com>
2025-05-26 17:01:50 -0500

    estructura parcial

---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public')); // carga directamente las paginas q se encuentren en public
+
+app.use(express.urlencoded({ extended: true })); // Permite leer los datos del formulario
+
+const fs = require('fs');
+const path = require('path');
+
+app.post('/submit', (req, res) => {
+  const { id, nombre, apellido, titulo, autor, editorial, año } = req.body;
+
+  // valida si falta algún dato, redirigir a error.html
+  if (!id || !nombre || !apellido || !titulo || !autor || !editorial || !año) {
+    return res.redirect('/error.html');
+  }
+
+  // crea el archivo txt, con la estrucutura y le coloca el id dependiendo del numero
+  const contenido = `${id},${nombre},${apellido},${titulo},${autor},${editorial},${año}`;
+  const nombreArchivo = `id_${id}.txt`;
+  const rutaArchivo = path.join(__dirname, 'data', nombreArchivo);
+
+  // Escribir el archivo
+  fs.writeFile(rutaArchivo, contenido, (err) => {
+    if (err) {
+      return res.status(500).send('Error al guardar el archivo.');
+    }
+
+    // Descargar el archivo
+    res.download(rutaArchivo, nombreArchivo);
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
